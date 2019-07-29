3084199ca3d9c2cef1a2f8c459e02c6f7026979b
Volker Hilsenstein <volker.hilsenstein@gmail.com>
2019-07-29 15:08:45 +1000

    additional speedup on shift calculation by avoiding duplicate fft calculations

--- notebooks/stabilize_sequence.py
+++ notebooks/stabilize_sequence.py
@@ -1,7 +1,26 @@
-import imreg
+#import imreg
 import numpy as np
 from skimage import transform
 from concurrent.futures import ThreadPoolExecutor
+
+
+def get_translation(f0, f1):
+    """ given the ffts of 2 images, 
+        calculate the translation of the two images
+
+        based on imreg.translation from Chris Gohlke
+        but modified to use pre-computed ffts
+        (avoids computing ffts twice)
+    """
+    shape = f0.shape
+
+    ir = abs(np.fft.ifft2((f0 * f1.conjugate()) / (abs(f0) * abs(f1))))
+    t0, t1 = np.unravel_index(np.argmax(ir), shape)
+    if t0 > shape[0] // 2:
+        t0 -= shape[0]
+    if t1 > shape[1] // 2:
+        t1 -= shape[1]
+    return [t0, t1]
 
 def stabilize_getshifts(im):
     """get shifts for sequence
@@ -13,12 +32,13 @@ def stabilize_getshifts(im):
     returns np.array of shifts
     """
     with ThreadPoolExecutor() as p:
-        shifts= list(p.map(lambda n: imreg.translation(im[n,...], im[n+1,...]), range(im.shape[0]-1)))
+        ffts = list(p.map(lambda i: np.fft.fft2(im[i,...]),range(im.shape[0])))
+        shifts= list(p.map(lambda n: get_translation(ffts[n], ffts[n+1]), range(im.shape[0]-1)))
     shifts=[[0,0]] + shifts
     return np.array(shifts)
 
 def stabilize_apply_shifts(seq, shifts):
-    """apply shifts to a sequence possibly with multiple channels  
+    """apply shifts to a sequence (possibly with multiple channels)  
     Parameters
     ----------
     seq : numpy array time series (tyx or tcyx)
@@ -49,9 +69,9 @@ def stabilize_apply_shifts(seq, shifts):
     else:
         return padded_series
 
-    def stabilize_crop_to_overlap(seq):
-        tmp = np.where(np.min(seq, axis=0)>0)
-        miny, maxy = np.min(tmp[-2]),np.max(tmp[-2])
-        minx, maxx = np.min(tmp[-1]),np.max(tmp[-1])
-        cropped = seq[..., miny:maxy, minx:maxx]
-        return cropped
+def stabilize_crop_to_overlap(seq):
+    tmp = np.where(np.min(seq, axis=0)>0)
+    miny, maxy = np.min(tmp[-2]),np.max(tmp[-2])
+    minx, maxx = np.min(tmp[-1]),np.max(tmp[-1])
+    cropped = seq[..., miny:maxy, minx:maxx]
+    return cropped
