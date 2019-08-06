d55ed25fd55d11553e6c20963d1bf8f8c4e9d305
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-08-06 00:21:59 -0500

    Add docstring to the metadata extraction function

--- notebooks/meta.py
+++ notebooks/meta.py
@@ -11,7 +11,32 @@ def channel_metadata(series_filename,
         scratch_prefix='/scratch/dk49',
         project_prefix='/projects/dk49',
         return_timepoints=False):
-    """Get metadata from a series filename."""
+    """Get metadata from a series filename.
+    
+    Parameters
+    ----------
+    series_filename : str
+        The filename of the series to process.
+    scratch_prefix : str, optional
+        The base location of the tiff series in /scratch.
+    project_prefix : str, optional
+        The base location of the nd2 files in /projects.
+    return_timepoints : bool, optional
+        Whether to return the timestamps in milliseconds. This *could*
+        crash the process depending on the nd2 file.
+
+    Returns
+    -------
+    series_id : int
+        The series ID within the original file.
+    stage_coords : dict with keys 'x_um' and 'y_um'.
+        The stage coordinates for this series.
+    channel_info : list of dict with keys 'name', 'emission_nm', 'rgb_value'
+        The information for each channel in the series.
+    timepoints_ms : list of float, or None
+        The timepoint of each image in the series, in ms. Is None if
+        ``return_timepoints`` is False.
+    """
     series_filename = os.path.abspath(series_filename)
     original_filename = series_filename.replace(scratch_prefix,
                                                 project_prefix)
